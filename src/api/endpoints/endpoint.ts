876cc4683b769d2aa25a23865308ca6ece4c7284
Austin.Hoang <austin.hoang@avepoint.com>
2024-04-18 20:58:29 +0700

    Init project

---
+++ src/api/endpoints/endpoint.ts
@@ -0,0 +1,5 @@
+export default class Endpoint {
+  public static get baseUrl(): string {
+    return "http://localhost:5244/api/";
+  }
+}
---
+++ src/api/middleware/middleware.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useNavigate } from "react-router";
+import Endpoint from "../endpoints/endpoint";
+
+// Create an Axios instance
+export const axiosInstance = axios.create({
+  baseURL: Endpoint.baseUrl, // Replace with your API's base URL
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      config.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  async function (error) {
+    if (error?.response?.status === 401) {
+      const navigate = useNavigate();
+      navigate("/login");
+    }
+    return Promise.reject(error);
+  }
+);
